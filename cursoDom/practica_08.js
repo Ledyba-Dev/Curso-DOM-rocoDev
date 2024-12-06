da19c8e5f5046d611398048f53d17dd2ad551a81
Rodri <rodrigovizcarra9623@gmail.com>
2024-12-05 21:53:32 -0300

    Codigo inicial del curso

---
+++ cursoDom/practica_08.js
@@ -0,0 +1,23 @@
+/* 
+El objetivo de este ejercicio es tener un boton que al darle click
+cambie el color de fondo de toda la página por un color aleatorio
+
+*/
+
+
+
+/* 
+El objetivo de este ejercicio es que creemo un menú de navegación
+responsivo y que al darle click en el icono de menú hamburguesa
+se muestre la barra de navegación y si damos otro click que se oculte
+
+*/
+
+
+
+
+/* 
+Crea un componente tab, que tenga tres prestañas y que al darle click
+a una de sus pestañas, se muestres el contenido perteneciente a 
+dicha pestaña
+*/
